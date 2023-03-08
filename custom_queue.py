c750c07ad56dfbe3b668f5c6c217003e0c9b04a2
irinaloskutova <l.irinka@mail.ru>
2023-03-08 19:25:20 +0500

    курс 4. домашка 3. задание 3 (tests)

--- custom_queue.py
+++ custom_queue.py
@@ -18,13 +18,13 @@ class Queue:
             self.tail.next_node = new_node
             self.tail = new_node
 
-queue = Queue()
-queue.enqueue('data1')
-queue.enqueue('data2')
-queue.enqueue('data3')
-
-print(queue.head.data)
-print(queue.head.next_node.data)
-print(queue.tail.data)
-print(queue.tail.next_node)
-print(queue.tail.next_node.data)
+# queue = Queue()
+# queue.enqueue('data1')
+# queue.enqueue('data2')
+# queue.enqueue('data3')
+#
+# print(queue.head.data)
+# print(queue.head.next_node.data)
+# print(queue.tail.data)
+# print(queue.tail.next_node)
+# print(queue.tail.next_node.data)
